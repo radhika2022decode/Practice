f073fa28544caa63f740924ed2eabc594673790a
DESKTOP-D80795R\Radhika Gupta <guptaradhika2003@gmail.com>
2022-09-25 01:31:51 +0530

    dggfg

---
+++ IdeaProjects/learningJava/src/inh/BMW.java
@@ -0,0 +1,11 @@
+package inh;
+
+public class BMW extends Car2{
+    BMW(int x, int y) {
+        super(x,y);
+        System.out.print("This Is Para BMW ");
+    }
+    void BMW() {
+        System.out.print("This Is Def BMW");
+    }
+}
